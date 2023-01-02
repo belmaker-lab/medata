c66159d59f8ba1001c683fb876d9a18b08cb95cd
Shira Salingre <salingre@mail.tau.ac.il>
2023-01-02 16:26:17 +0100

    ignoring bio oracle data which contains large files

--- R/bio-oracle extraction code.R
+++ R/bio-oracle extraction code.R
@@ -9,18 +9,24 @@ library(tidyverse)
 medata <- read_rds("data/medata.Rds")
 
 # explore layers from Bio ORACLE
-all_bio_layers <- list_layers(datasets = "Bio-ORACLE", marine = TRUE) %>%
+all_bio_layers <- sdmpredictors::list_layers(datasets = "Bio-ORACLE", marine = TRUE) %>%
   dplyr::select(layer_code, name) # to view layers names and code
 
 # to search for a layer code by a specific string - change the 2nd arg in str_detect:
 all_bio_layers %>% filter(stringr::str_detect(.$name, "temperature"))
+all_bio_layers %>% filter(stringr::str_detect(.$name, "salinity"))
+all_bio_layers %>% filter(stringr::str_detect(.$name, "Primary production"))
 
 # Download environmental layers -------------------------------------------
 
 my.sites <- medata %>% dplyr::select(site, lon, lat)
-environment.layers <- load_layers(layercodes = c("BO_sstmean", "BO_sstrange", # temperature
+
+environment.layers <- sdmpredictors::load_layers(layercodes = c("BO_sstmean", # temperature
                                                  "BO2_salinitymean_ss",  # salinity
-                                                 "BO2_ppmean_ss", "BO2_pprange_ss")) # productivity
+                                                 "BO22_ppmean_ss"), # productivity
+                                                 datadir = "data/bio_oracle")
+# # If there's an error with file downloading due to timeout limit, run the following code then attempt to download layers again:
+# options(timeout = max(300, getOption("timeout")))
 
 # Extract environmental values for sites in medata ------------------------
 
@@ -29,30 +35,9 @@ my.sites.environment <- data.frame(site = my.sites$site,
 my.sites.environment
 
 # Check for NAs
-my.sites.environment %>% dplyr::filter(!complete.cases(.)) %>% 
+my.sites.environment %>% 
+  dplyr::filter(!complete.cases(.)) %>% 
   dplyr::distinct() %>% summarise(sites_with_NA = n())
 
-# Merge with Medata -------------------------------------------------------
-colnames(medata)
 
-medata %<>%
-  left_join(my.sites.environment, by = "site") %>% # join with new env. data
-  select(c("data.origin", "country", "site", "trans", "lon", "lat", "season", # tidy up
-           "species", "sp.length", "sp.n",
-           "protection", "enforcement", "total.mpa.ha", "size.notake", "yr.creation", "age.reserve.yr",
-           "a", "b", "depth",
-           "tmean" = "BO_sstmean", "trange" = "BO_sstrange",
-           "sal_mean" = "BO2_salinitymean_ss",
-           "pp_mean" = "BO2_ppmean_ss", "pp_range" = "BO2_pprange_ss")) %>% 
-  mutate_at(.vars = c("data.origin", "country", "site", "trans", "season", "species", "enforcement"),
-            .funs = as_factor)
 
-# Check for NAs per variables
-medata %>% dplyr::filter(!complete.cases(sal_mean)) %>% dplyr::distinct()
-
-## I'm not saving this yet, because I don't want to override NAs onto
-## existing data in the original medata file, so for now I'll just export the salinity part
-new_salinity <- medata %>% dplyr::select(site, trans, lat, lon, sal_mean)
-# write_csv(new_salinity, "data/new_salinity_vals.csv")
-
-# write_rds("data/medata.Rds")
