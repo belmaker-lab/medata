a9f7182b2f6c2d3c7bcecf4aef283f70571ddf3f
Shira Salingre <salingre@mail.tau.ac.il>
2023-01-02 17:18:35 +0100

    updated wrangling script for better coherence

--- R/wrangle_medata.R
+++ R/wrangle_medata.R
@@ -1,46 +1,43 @@
 library(magrittr)
 library(tidyverse)
-library(rfishbase)
 
-# -------------------------------------------------------------------------
+# The original data used here has been gathered and cleaned by Ori Frid and Shira Salingre (Balmaker lab).
+# This script is updated each time the whole dataset requires a correction or addition.
 
-med_raw <- read_csv("data/uvc_data_19022020.csv", col_types = cols(depth = "d"))
-summary(med_raw)
-
+medata <- read_csv("data/uvc_data_19022020.csv", col_types = cols(depth = "d"))
+summary(medata)
 
 # Correct seasons ---------------------------------------------------------
 
 # Season exhibits 4 levels, 2 of which are the same (`Fall` and `Autumn`)
 
-med_raw %>% distinct(season)
+medata %>% distinct(season)
 
-med_raw <- med_raw %>% mutate(season = if_else(season == "Fall", "Autumn", season))
-as.factor(med_raw$season) %>% levels()
-
+medata <- medata %>% mutate(season = if_else(season == "Fall", "Autumn", season))
+as.factor(medata$season) %>% levels()
 
 # Convert case ------------------------------------------------------------
 
-med_raw <- med_raw %>% mutate(site = str_to_lower(site), season = str_to_lower(season))
-
+medata <- medata %>% mutate(site = str_to_lower(site), season = str_to_lower(season))
 
 # Remove whitespaces ------------------------------------------------------
 
 # Species names
-med_raw <- med_raw %>% mutate(species = str_trim(species, "both"))
+medata <- medata %>% mutate(species = str_trim(species, "both"))
 
 # Sites (plus remove an unneeded `,` in one of the site names):
-med_raw <- med_raw %>% mutate(site = str_replace_all(site, "[[:space:]]", "\\_"), site = str_remove_all(site, "\\,"))
+medata <- medata %>% mutate(site = str_replace_all(site, "[[:space:]]", "\\_"), site = str_remove_all(site, "\\,"))
 
 
 # MPAs --------------------------------------------------------------------
 
 # Change `protection` to logical
-table(med_raw$protection) # just to make sure it works fine
-med_raw <- med_raw %>% mutate(protection = if_else(protection == "NO", FALSE, TRUE))
-table(med_raw$protection)
+table(medata$protection) # just to make sure it works fine
+medata <- medata %>% mutate(protection = if_else(protection == "NO", FALSE, TRUE))
+table(medata$protection)
 
 # Sites that are outside of MPAs (protection == FALSE) should have `total.mpa.ha` and `size.notake` set to NA
-med_raw <- med_raw %>% mutate(total.mpa.ha = ifelse(protection == FALSE & total.mpa.ha == 0, NA, total.mpa.ha),
+medata <- medata %>% mutate(total.mpa.ha = ifelse(protection == FALSE & total.mpa.ha == 0, NA, total.mpa.ha),
                               size.notake = ifelse(protection == FALSE & size.notake == 0, NA, size.notake))
 
 
@@ -48,99 +45,57 @@ med_raw <- med_raw %>% mutate(total.mpa.ha = ifelse(protection == FALSE & total.
 # Fix depth data from three transects in Crete, where there are 2 depth values:
 
 # First find them:
-med_raw %>%
+medata %>%
   dplyr::select(site, lon, lat, trans, depth) %>%
   distinct() %>% 
   count(site,lon,lat,trans) %>%
   arrange(desc(n))
 
 # ...Check them out...
-med_raw %>%
+medata %>%
   dplyr::select(site, lon, lat, trans, depth) %>%
   distinct() %>%
-  filter(site == "assecret2210191mlsc_b")
+  filter(site == "assecret2210191mlsc_a" | site == "assecret2210191mlsc_b" | site == "assecret2210191mlsc_c") %>% 
+  arrange(site)
 
 # Then fix them
-med_raw[which(med_raw$site == "assecret2210191mlsc_a"),]$depth %<>% mean()
-med_raw[which(med_raw$site == "assecret2210191mlsc_b"),]$depth %<>% mean()
-med_raw[which(med_raw$site == "assecret2210191mlsc_c"),]$depth %<>% mean()
-
-
-# write_rds(med_raw, "data/med_raw.Rds")
+medata[which(medata$site == "assecret2210191mlsc_a"),]$depth %<>% mean()
+medata[which(medata$site == "assecret2210191mlsc_b"),]$depth %<>% mean()
+medata[which(medata$site == "assecret2210191mlsc_c"),]$depth %<>% mean()
 
 
 # Add species information -------------------------------------------------
+# data from FishBase (see 'R/fishbase_info_addition.R')
 
-medata <- read_rds("data/med_raw.Rds")
 
-species_info <- medata %>% 
+## Create a species information dataset ------------------------------------
+species_list <- medata %>% 
   distinct(species) %>% 
   mutate(species = str_replace(species, "\\.", "\\ ")) %>% 
   filter(!str_detect(species, "dae")) %>% 
   filter(!str_detect(species, "spp"))
 
-species_info %>% arrange(species) %>% print(n = Inf)
+species_list %>% arrange(species) %>% print(n = Inf)
 
 
-# Add diet information ----------------------------------------------------
+## Add trophic information -------------------------------------------------
 
-species_diet <- ecology(species_info$species, 
+species_diet <- rfishbase::ecology(species_list$species, 
                         fields = c("Species", "DietTroph", "DietSeTroph", "FoodTroph", "FoodSeTroph"))
 
-# Which metric (DietTroph/FoodTroph) has more NAs?
-sapply(species_diet, function(x) sum(is.na(x)))
-
-species_diet %>% 
-  filter(is.na(FoodTroph))
-
 # Species with missing FoodTroph also have missing DietTroph (except Parablennius zvonimiri)
-# so we'll keep 'FoodTroph'
+# so we'll keep 'FoodTroph' as it has less NAs (more information)
 species_troph <- species_diet %>% 
   mutate(species = str_replace(Species, "\\ ", "\\.")) %>% 
   select(species, FoodTroph, FoodSeTroph)
 
-species_troph
-
-# Join
 medata <- medata %>% left_join(species_troph)
 
-# Lessepsian migrants -----------------------------------------------------
-
-indie_species <- read_csv("data/exotic_species.csv")
-
-medata <- medata %>% left_join(indie_species)
-
-# Add Length-Weight ratio constants ---------------------------------------
-
-length_weight(species_info$species) %>% colnames()
-
-missing_constants <- medata %>%
-  filter(is.na(a)) %>%
-  filter(is.na(b)) %>% 
-  distinct(species) %>% 
-  mutate(species = str_replace(.$species, pattern = "\\.", "\\ "))
-
-species_LW <- length_weight(missing_constants$species, 
-                            fields = c("Species", "a", "b", "Type", "Method")) %>% 
-  filter(Method == "type I linear regression" & Type == "TL" | Method == "Type I linear regression" & Type == "TL") %>% 
-  group_by(Species) %>% summarise(mean_a = mean(a), mean_b = mean(b)) %>% ungroup() %>% 
-  mutate(species = str_replace(.$Species, pattern = "\\ ", "\\.")) %>% 
-  select(-Species)
-
-# Add to medata
-
-medata <- medata %>% 
-  left_join(species_LW) %>% 
-  mutate(a = if_else(is.na(a), mean_a, a),
-         b = if_else(is.na(b), mean_b, b)) %>% 
-  select(-mean_a, -mean_b)
-
-
-# Add family --------------------------------------------------------------
+## Add family --------------------------------------------------------------
 
 species_family <- rfishbase::load_taxa() %>% 
   as_tibble() %>% 
-  filter(Species %in% species_info$species) %>%
+  filter(Species %in% species_list$species) %>%
   select(Species, Family) %>% 
   mutate(species = str_replace(.$Species, pattern = "\\ ", "\\.")) %>% 
   select(species, family = Family)
@@ -160,17 +115,76 @@ medata <- medata %>% mutate(family = case_when(str_detect(species, "Symphodus") 
                                                str_detect(species, "Tripterygion") ~ "Tripterygiidae",
                                                TRUE ~ as.character(family)))
 
+# Lessepsian migrants -----------------------------------------------------
+
+indie_species <- read_csv("data/exotic_species.csv")
+
+medata <- medata %>% left_join(indie_species)
+
+# Add Length-Weight ratio constants ---------------------------------------
+# relevant columns 'a' and 'b'
+rfishbase::length_weight(species_list$species) %>% colnames()
+
+missing_constants <- medata %>%
+  filter(is.na(a)) %>%
+  filter(is.na(b)) %>% 
+  distinct(species) %>% 
+  mutate(species = str_replace(.$species, pattern = "\\.", "\\ "))
+
+species_LW <- rfishbase::length_weight(missing_constants$species, 
+                            fields = c("Species", "a", "b", "Type", "Method")) %>% 
+  filter(Method == "type I linear regression" & Type == "TL" | Method == "Type I linear regression" & Type == "TL") %>% 
+  group_by(Species) %>% summarise(mean_a = mean(a), mean_b = mean(b)) %>% ungroup() %>% 
+  mutate(species = str_replace(.$Species, pattern = "\\ ", "\\.")) %>% 
+  select(-Species)
+
+# Add to medata
+
+medata <- medata %>% 
+  left_join(species_LW) %>% 
+  mutate(a = if_else(is.na(a), mean_a, a),
+         b = if_else(is.na(b), mean_b, b)) %>% 
+  select(-mean_a, -mean_b)
+
 
 # Better site names for Crete expedition ----------------------------------
-# The Crete 'sites' are actually transects here. Separate them (+ remove data that is problematic from 'azz_as')
+# The Crete 'sites' are actually transects here. Separate them
 
 medata <- medata %>% 
   mutate(site = case_when(str_starts(site, "assecret") ~ 
                             str_extract(site, "assecret\\d\\d\\d\\d\\d\\d\\d"),
-                          TRUE ~ as.character(site))) %>% 
-  filter(data.origin != "azz_asi")
+                          TRUE ~ as.character(site)))
 
-# Save --------------------------------------------------------------------
+
+# Add environmental data --------------------------------------------------
+
+# Remove pre-existing environmental data (if needed)
+medata %>% colnames()
+medata <- medata %>% select(-c("tmean", "trange", "sal_mean", "pp_mean", "pp_range"))
+
+gc() # clear the memory for handling large files
+
+my.sites <- medata %>% dplyr::select(site, lon, lat)
+environment.layers <- sdmpredictors::load_layers(layercodes = c("BO_sstmean", # temperature
+                                                                "BO2_salinitymean_ss",  # salinity
+                                                                "BO22_ppmean_ss"), # productivity
+                                                 datadir = "data/bio_oracle") 
+raster::plot(environment.layers)
+
+my.sites.environment <- bind_cols(my.sites, 
+                                  raster::extract(environment.layers, my.sites[,2:3]))
+
+# Check for NAs
+my.sites.environment %>% 
+  dplyr::filter(!complete.cases(.)) %>% 
+  dplyr::distinct() %>% summarise(sites_with_NA = n())
+
+
+medata <- medata %>%
+  left_join(my.sites.environment)
+
+
+# Final checks and save ---------------------------------------------------
 
 skimr::skim(medata)
 summary(medata)
@@ -180,7 +194,7 @@ medata <- medata %>%
                           "age.reserve.yr", "species", "family"), .fns = as.factor)) %>% 
   select(data.origin, country, season, lon, lat, site, trans, 
          protection, enforcement, total.mpa.ha, size.notake, yr.creation, age.reserve.yr,
-         depth, tmean, trange, sal_mean, pp_mean, pp_range, 
+         depth, tmean = BO_sstmean, sal_mean = BO2_salinitymean_ss, pp_mean = BO22_ppmean_ss, 
          species, sp.n, sp.length, a, b, family, exotic, FoodTroph, FoodSeTroph)
 
 
